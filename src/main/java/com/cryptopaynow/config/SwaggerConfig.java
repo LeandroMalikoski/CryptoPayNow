65ba88d13c969a899019c603e81ba6166528c5a6
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-09 19:57:52 -0300

    added packages and swagger config

---
+++ src/main/java/com/cryptopaynow/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.cryptopaynow.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Crypto Pay Now")
+                        .version("1.0.0")
+                        .description("Crypto Pay Now"));
+    }
+}
