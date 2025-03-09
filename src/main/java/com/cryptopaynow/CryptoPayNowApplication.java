96cb608839858eaa118a5396d6fb0933ca0ccac7
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-09 19:54:18 -0300

    first commit

---
+++ src/main/java/com/cryptopaynow/CryptoPayNowApplication.java
@@ -0,0 +1,13 @@
+package com.cryptopaynow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPayNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPayNowApplication.class, args);
+	}
+
+}
